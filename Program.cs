74076415597e4e8ad54a17c77291f1cd54ba0146
Taufan Augusta <taufan.augusta@protonmail.com>
2023-01-21 11:21:23 +0700

    configure logging

--- Program.cs
+++ Program.cs
@@ -3,6 +3,15 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+
 builder.Host.ConfigureServices((context, services) =>
 {
     services.AddRouteEndpoints(Assembly.GetExecutingAssembly());
--- Usings.cs
+++ Usings.cs
@@ -1,3 +1,4 @@
 global using m0.api.Extensions;
 global using m0.api.Interfaces;
 global using m0.api.Services;
+global using Serilog;
